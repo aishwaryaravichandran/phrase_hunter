19e28cdf028ba0595c071b1cf15f57f199f353d0
aishwaryaravichandran <aishwarya10397@gmail.com>
2019-06-18 21:33:15 +0000

    Main File

---
+++ app.py
@@ -0,0 +1,11 @@
+from game import Game
+
+if __name__ == "__main__":
+    
+    word_game = Game(['spiderman', 'superman', 'batman', 'flash', 'wonderwoman', 'thor', 'groot', 'avengers'])
+    word_game.start_game()
+
+
+
+
+
